67c11607e80d93cfc3757a8407e074d2c037a851
zsu <azure.ss@gmail.com>
2018-09-26 19:29:46 -0400

    fix multiple db contexts bug

--- GenericRepository/Uow/IUowProvider.cs
+++ GenericRepository/Uow/IUowProvider.cs
@@ -7,5 +7,6 @@ namespace GenericRepository
     {
         IUnitOfWork CreateUnitOfWork(bool trackChanges = true, bool enableLogging = false);
         IUnitOfWork CreateUnitOfWork<TEntityContext>(bool trackChanges = true, bool enableLogging = false) where TEntityContext : DbContext;
-    }
+        IUnitOfWork CreateUnitOfWork(DbContext context, bool trackChanges = true, bool enableLogging = false);
+   }
 }
