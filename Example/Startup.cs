4943ac7756d294895a8966f0a06955a54bd5ffbe
zsu <azure.ss@gmail.com>
2019-02-14 21:01:42 -0500

    Upgrade to .net core .2.2

--- Example/Startup.cs
+++ Example/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
-using GenericRepository;
+using GenericRepository.Mvc;
 using Microsoft.Extensions.Options;
 using System.IO;
 
