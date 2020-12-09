bdfbb53f5ff00eedb071dc4b374bea44a6bd1a43
zsu <azure.ss@gmail.com>
2020-12-08 21:16:56 -0500

    upgrade to netcore 5

--- GenericRepository/Context/ModelBuilderExtensions.cs
+++ GenericRepository/Context/ModelBuilderExtensions.cs
@@ -18,14 +18,14 @@ namespace GenericRepository
                 }
 
                 // Set the table name to the (simple) name of the CLR type and lowercase it
-                entityType.Relational().TableName = entityType.ClrType.Name.ToLower();
+                entityType.SetTableName(entityType.ClrType.Name.ToLower());
 
                 // Lowercase all properties
                 foreach ( var property in entityType.GetProperties() )
                 {
                     //Check if property has a ColumnAttribute. If so, use the lowercased value of this attribute instead of lowercased property name
                     var columnNameAttribute = property.GetAnnotations().FirstOrDefault(x => x.Name.ToLower().Contains("columnname"))?.Value;
-                    property.Relational().ColumnName = columnNameAttribute == null ? property.Name.ToLower() : columnNameAttribute.ToString().ToLower();
+                    property.SetColumnName(columnNameAttribute == null ? property.Name.ToLower() : columnNameAttribute.ToString().ToLower());
                 }
             }
         }
