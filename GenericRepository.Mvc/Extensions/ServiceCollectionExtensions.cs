4f8078ca87c5c75b9b377f4ac3ff116cb6d21f94
zsu <azure.ss@gmail.com>
2022-10-18 09:20:50 -0400

    Refactor

--- GenericRepository.Mvc/Extensions/ServiceCollectionExtensions.cs
+++ GenericRepository.Mvc/Extensions/ServiceCollectionExtensions.cs
@@ -15,7 +15,7 @@ namespace GenericRepository.Mvc
 
         private static void RegisterDataAccess<TEntityContext>(IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction) where TEntityContext : DbContext,IEntityContext//EntityContextBase<TEntityContext>
         {
-            services.TryAddSingleton<IUowProvider, UowProvider>();
+            services.TryAddTransient<IUowProvider, UowProvider>();
             DbContextOptionsBuilder<TEntityContext> optionBuilder = new DbContextOptionsBuilder<TEntityContext>();
             if (optionsAction != null)
             {
